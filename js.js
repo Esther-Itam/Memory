b1538b655ce234bf50cf3bbcadc51152898aa19d
Esther-Itam <63000986+Esther-Itam@users.noreply.github.com>
2020-06-03 17:03:13 +0200

    Add files via upload

--- js.js
+++ js.js
@@ -1,53 +1,121 @@
-var button = document.querySelector("button");
-button.addEventListener('click', function(){
-    console.log("salut");
-    var unicode = document.querySelectorAll(".game-card");
-    unicode.sort(function(){
-        return .5-Math.random();
-        });
-    
-   
-})
-  
+class Game{
 
-var cards = document.querySelectorAll(".game-card");
-var cardFace;
-var cardBack;
-var flippedCard = false;
-var lock = false;
+    constructor(){
+this.cards = document.querySelectorAll(".game-card");
+this.cardFace;
+this.cardBack;
+this.flippedCard = false;
+this.lock = false;
+this.button = document.querySelector("button");
 
-function flipCard(){
+}
+
+flipCard(){
+    if(lock) return;
    this.classList.toggle('flip');
   
    if(!flippedCard){
-       flippedCard = true;
-       cardFace = this;
+       this.flippedCard = true;
+       this.cardFace = this;
    }
    else{
-    flippedCard = false;
-    cardBack = this; 
+    this.flippedCard = false;
+    this.cardBack = this; 
 
-    if(cardFace.dataset.class === cardBack.dataset.class){
-        cardFace.removeEventListener('click', flipCard);
-        cardBack.removeEventListener('click', flipCard);
+
+    if(this.cardFace.dataset.card === this.cardBack.dataset.card){
+        this.cardFace.removeEventListener('click', flipCard);
+        this.cardBack.removeEventListener('click', flipCard);
     }else{
+        this.lock = true;
         setTimeout(function(){
-        cardFace.classList.remove('flip');
-        cardBack.classList.remove('flip');
+        this.cardFace.classList.remove('flip');
+        this.cardBack.classList.remove('flip');
+        this.lock = false;
+        console.log("cardFace");
 
     }, 1500);}
    }
   
 }
 
-cards.forEach(card => card.addEventListener('click', flipCard));
 
 
+// random cards après victoire
 
-/*var unicode = ['🎈', '🎄', '🎺', '🐫','🎈', '🎄', '🎺', '🐫'];
-    unicode.sort(function(){
-    return .5-Math.random();
-    });
-    document.write(unicode);
-    console.log(unicode);*/
+shuffle(){
     
+    this.cards.forEach(card => {
+        var random = Math.floor(Math.random()*8);
+        card.style.order = random;
+        console.log("salut");
+    })
+}
+
+
+
+}
+ 
+let game = new Game()
+cards.forEach(card => card.addEventListener('click', flipCard));
+button.addEventListener('click', shuffle); 
+
+
+
+
+/*// timer
+var span = document.getElementsByTagName("span");
+var time;
+var ms=0,s=0,mn=0,h=0;
+
+
+function start(){
+    time =setInterval(update,100);
+    cards.disabled=true;
+   console.log("salut");
+   }
+
+   cards.forEach(card => card.addEventListener('click', start));
+
+   function update(){
+     ms+=1;
+        if(ms==10){
+         ms=1;
+         s+=1;
+        }
+        if(s==60){
+         s=0;
+         mn+=1;
+        }
+        if(mn==60){
+         mn=0;
+         h+=1;
+        }
+
+        span[0].innerHTML=h+" h";
+        span[1].innerHTML=mn+" min";
+        span[2].innerHTML=s+" s";
+        span[3].innerHTML=ms+" ms";
+
+    }
+ 
+
+      function stop(){
+        clearInterval(time);
+        button.disabled=false;
+
+        }
+        button.addEventListener('click', stop);
+
+        function reset(){
+            clearInterval(time);
+            cards.disabled=false;
+            ms=0,s=0,mn=0,h=0;
+            
+            span[0].innerHTML=h+" h";
+            span[1].innerHTML=mn+" min";
+            span[2].innerHTML=s+" s";
+            span[3].innerHTML=ms+" ms";
+          }
+          cards.addEventListener('click', reset); 
+*/
